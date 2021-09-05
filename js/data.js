8174e21df3e0744d3a40f3d046c9d7fdec0f8bbe
bgocean <51014032+bgocean@users.noreply.github.com>
2021-09-05 16:48:21 +0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,35 @@
+const data = {
+    "first_name": "Sophia",
+    "last_name": "Hawthorne",
+    "job_title": "MARKETING DIRECTOR",
+    "photo": "images/w1.jpg",
+    "phone": "+44 20 55-555-555",
+    "email": "hawthorne@gmail.com",
+    "skype": "skype: S.Hawthorne",
+    "linkedin": "uk.linkedin.com/in/hawthorne",
+    "address": "221B Baker Street, London",
+    "skills": [
+        "Creative Development",
+        "Branding & Identity",
+        "Project Management",
+        "Contact Negotiation",
+        "Ad Buying & Planning",
+        "Product Positioning",
+        "Visual Merchandising",
+        "Search Engine Ads",
+        "Process Improvement",
+        "Market Strategizing",
+        "Adobe Creative Suite",
+        "Microsoft Office 2016",
+        "Google Web Analytics",
+        "Fluent in Spanish",
+    ],
+    "education": [
+        ["2008-2010", "University of Alabama English Language Institute"],
+        ["2010-2015", "University of Alabama English Language Institute"],
+    ],
+    "achievements": [
+        "Marketer of the Year Academic Scholorship Deans List: 2006-2007, National Merit Scholarship Corporation"
+    ],
+    "profile": "This paragraph will be you first impressions to recruiters. An effective summary is short, concise and focuses on your most important selling points. You should align your summary with the job requirements by highlighting the key strengths and skills that will truly make you stand out as a highly qualified candidate. Transforming responsibilities into quantifiable achievements with numbers communicates your value."
+};
